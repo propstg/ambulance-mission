913c959380895d8c19593eb57d861f2e771c6060
gregory propst <gregory.propst@gmail.com>
2019-04-28 23:20:01 -0400

    add coverage for scaleforms

--- src/client/scaleform.lua
+++ src/client/scaleform.lua
@@ -1,73 +1,73 @@
 Scaleform = {}
 
 function Scaleform.ShowPassed()
-    Scaleform.ShowWasted(_('terminate_won'), '', 5)
+    Scaleform.ShowWasted(Wrapper._('terminate_won'), '', 5)
 end
 
 function Scaleform.ShowWasted(message, subMessage, secondsToShow)
     Citizen.CreateThread(function()
-        local scaleform = Scaleform.LoadScaleform('MP_BIG_MESSAGE_FREEMODE')
+        local scaleform = Scaleform.loadScaleform('MP_BIG_MESSAGE_FREEMODE')
 
-        BeginScaleformMovieMethod(scaleform, 'SHOW_SHARD_WASTED_MP_MESSAGE')
-        PushScaleformMovieMethodParameterString(message)
-        PushScaleformMovieMethodParameterString(subMessage)
-        PushScaleformMovieMethodParameterInt(5)
-        EndScaleformMovieMethod()
+        Wrapper.BeginScaleformMovieMethod(scaleform, 'SHOW_SHARD_WASTED_MP_MESSAGE')
+        Wrapper.PushScaleformMovieMethodParameterString(message)
+        Wrapper.PushScaleformMovieMethodParameterString(subMessage)
+        Wrapper.PushScaleformMovieMethodParameterInt(5)
+        Wrapper.EndScaleformMovieMethod()
 
-        Scaleform.Draw(scaleform, secondsToShow * 50, 0)
+        Scaleform.draw(scaleform, secondsToShow * 50, 0)
     end)
 end
 
 function Scaleform.ShowMessage(message, subMessage, secondsToShow)
     Citizen.CreateThread(function()
-        local scaleform = Scaleform.LoadScaleform('MIDSIZED_MESSAGE')
+        local scaleform = Scaleform.loadScaleform('MIDSIZED_MESSAGE')
 
-        BeginScaleformMovieMethod(scaleform, 'SHOW_MIDSIZED_MESSAGE')
-        PushScaleformMovieMethodParameterString(message)
-        PushScaleformMovieMethodParameterString(subMessage)
-        EndScaleformMovieMethod()
+        Wrapper.BeginScaleformMovieMethod(scaleform, 'SHOW_MIDSIZED_MESSAGE')
+        Wrapper.PushScaleformMovieMethodParameterString(message)
+        Wrapper.PushScaleformMovieMethodParameterString(subMessage)
+        Wrapper.EndScaleformMovieMethod()
 
-        Scaleform.Draw(scaleform, secondsToShow * 50, 1)
+        Scaleform.draw(scaleform, secondsToShow * 50, 1)
     end)
 end
 
 function Scaleform.ShowAddTime(time)
-    Scaleform.ShowCountdown(time, 30, 30, 255)
+    Scaleform.showCountDown(time, 30, 30, 255)
 end
 
 function Scaleform.ShowRemoveTime(time)
-    Scaleform.ShowCountdown(time, 255, 30, 30)
+    Scaleform.showCountDown(time, 255, 30, 30)
 end
 
-function Scaleform.ShowCountdown(text, r, g, b)
+function Scaleform.showCountDown(text, r, g, b)
     Citizen.CreateThread(function()
-        local scaleform = Scaleform.LoadScaleform('COUNTDOWN')
+        local scaleform = Scaleform.loadScaleform('COUNTDOWN')
 
-        BeginScaleformMovieMethod(scaleform, 'SET_MESSAGE')
-        PushScaleformMovieMethodParameterString(text)
-        PushScaleformMovieMethodParameterInt(r)
-        PushScaleformMovieMethodParameterInt(g)
-        PushScaleformMovieMethodParameterInt(b)
-        PushScaleformMovieMethodParameterBool(false)
-        EndScaleformMovieMethod()
+        Wrapper.BeginScaleformMovieMethod(scaleform, 'SET_MESSAGE')
+        Wrapper.PushScaleformMovieMethodParameterString(text)
+        Wrapper.PushScaleformMovieMethodParameterInt(r)
+        Wrapper.PushScaleformMovieMethodParameterInt(g)
+        Wrapper.PushScaleformMovieMethodParameterInt(b)
+        Wrapper.PushScaleformMovieMethodParameterBool(false)
+        Wrapper.EndScaleformMovieMethod()
 
-        Scaleform.Draw(scaleform, 40, 10)
+        Scaleform.draw(scaleform, 40, 10)
     end)
 end
 
-function Scaleform.LoadScaleform(scaleformName)
-    local scaleform = RequestScaleformMovie(scaleformName)
+function Scaleform.loadScaleform(scaleformName)
+    local scaleform = Wrapper.RequestScaleformMovie(scaleformName)
 
-    while not HasScaleformMovieLoaded(scaleform) do
+    while not Wrapper.HasScaleformMovieLoaded(scaleform) do
         Citizen.Wait(0)
     end
 
     return scaleform
 end
 
-function Scaleform.Draw(scaleform, loops, delayBetweenLoops)
+function Scaleform.draw(scaleform, loops, delayBetweenLoops)
     for i = 1, loops do
-        DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
+        Wrapper.DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
         Citizen.Wait(delayBetweenLoops)
     end
 end
--- test/client/scaleform_spec.lua
+++ test/client/scaleform_spec.lua
@@ -1,13 +1,113 @@
 local mockagne = require 'mockagne'
+local when = mockagne.when
+local verify = mockagne.verify
 
 describe('client - scaleform', function()
     
-    setup(function()
+    local threadFunctionCaptor
+    local waitSpy
+
+    before_each(function()
+        _G.unpack = table.unpack
         _G.Wrapper = mockagne.getMock()
-        _G.Citizen = mockagne.getMock()
+        _G.Citizen = {
+            CreateThread = function(threadFunction)
+                threadFunctionCaptor = threadFunction
+            end,
+            Wait = function() end
+        }
+
+        waitSpy = spy.on(_G.Citizen, 'Wait')
 
         require('../../src/client/scaleform')
     end)
 
-    pending('TODO:  write tests for scaleform')
+    it('ShowPassed', function()
+        when(_G.Wrapper._('terminate_won')).thenAnswer('terminate_won translated')
+        when(_G.Wrapper.RequestScaleformMovie('MP_BIG_MESSAGE_FREEMODE')).thenAnswer('scaleformObject')
+        when(_G.Wrapper.HasScaleformMovieLoaded('scaleformObject')).thenAnswer(true)
+        
+        Scaleform.ShowPassed()
+
+        assert.is_not_nil(threadFunctionCaptor)
+        threadFunctionCaptor()
+
+        verify(_G.Wrapper.BeginScaleformMovieMethod('scaleformObject', 'SHOW_SHARD_WASTED_MP_MESSAGE'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString('terminate_won translated'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString(''))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(5))
+        verify(_G.Wrapper.EndScaleformMovieMethod())
+        verify(_G.Wrapper.DrawScaleformMovieFullscreen('scaleformObject', 255, 255, 255, 255))
+    end)
+
+    it('ShowWasted', function()
+        when(_G.Wrapper.RequestScaleformMovie('MP_BIG_MESSAGE_FREEMODE')).thenAnswer('scaleformObject')
+        when(_G.Wrapper.HasScaleformMovieLoaded('scaleformObject')).thenAnswer(true)
+        
+        Scaleform.ShowWasted('message', 'subMessage', 1)
+
+        assert.is_not_nil(threadFunctionCaptor)
+        threadFunctionCaptor()
+
+        verify(_G.Wrapper.BeginScaleformMovieMethod('scaleformObject', 'SHOW_SHARD_WASTED_MP_MESSAGE'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString('message'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString('subMessage'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(5))
+        verify(_G.Wrapper.EndScaleformMovieMethod())
+        verify(_G.Wrapper.DrawScaleformMovieFullscreen('scaleformObject', 255, 255, 255, 255))
+    end)
+
+    it('ShowMessage', function()
+        when(_G.Wrapper.RequestScaleformMovie('MIDSIZED_MESSAGE')).thenAnswer('scaleformObject')
+        when(_G.Wrapper.HasScaleformMovieLoaded('scaleformObject')).thenAnswer(true)
+        
+        Scaleform.ShowMessage('message', 'subMessage', 1)
+
+        assert.is_not_nil(threadFunctionCaptor)
+        threadFunctionCaptor()
+
+        verify(_G.Wrapper.BeginScaleformMovieMethod('scaleformObject', 'SHOW_MIDSIZED_MESSAGE'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString('message'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString('subMessage'))
+        verify(_G.Wrapper.EndScaleformMovieMethod())
+        verify(_G.Wrapper.DrawScaleformMovieFullscreen('scaleformObject', 255, 255, 255, 255))
+    end)
+
+    it('ShowAddTime', function()
+        when(_G.Wrapper.RequestScaleformMovie('COUNTDOWN')).thenAnswer('scaleformObject')
+        when(_G.Wrapper.HasScaleformMovieLoaded('scaleformObject')).thenAnswer(true)
+        
+        Scaleform.ShowAddTime(5)
+
+        assert.is_not_nil(threadFunctionCaptor)
+        threadFunctionCaptor()
+
+        verify(_G.Wrapper.BeginScaleformMovieMethod('scaleformObject', 'SET_MESSAGE'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString(5))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(30))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(30))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(255))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterBool(false))
+        verify(_G.Wrapper.EndScaleformMovieMethod())
+        verify(_G.Wrapper.DrawScaleformMovieFullscreen('scaleformObject', 255, 255, 255, 255))
+    end)
+
+    it('ShowRemoveTime', function()
+        when(_G.Wrapper.RequestScaleformMovie('COUNTDOWN')).thenAnswer('scaleformObject')
+        when(_G.Wrapper.HasScaleformMovieLoaded('scaleformObject')).thenAnswer(true)
+        
+        Scaleform.ShowRemoveTime(5)
+
+        assert.is_not_nil(threadFunctionCaptor)
+        threadFunctionCaptor()
+
+        verify(_G.Wrapper.BeginScaleformMovieMethod('scaleformObject', 'SET_MESSAGE'))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterString(5))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(255))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(30))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterInt(30))
+        verify(_G.Wrapper.PushScaleformMovieMethodParameterBool(false))
+        verify(_G.Wrapper.EndScaleformMovieMethod())
+        verify(_G.Wrapper.DrawScaleformMovieFullscreen('scaleformObject', 255, 255, 255, 255))
+    end)
 end)
